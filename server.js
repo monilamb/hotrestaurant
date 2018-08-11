8a77f2d4f2aaeae8bbab7e7eedf2b8a24df82c7d
MoniLamb <monica.lambrano@hotmail.com>
2018-08-11 10:32:21 -0400

    Routing done

--- server.js
+++ server.js
@@ -8,6 +8,34 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Routing
+var visitorCount = 0;
+
+app.get("/", function(req, res) {
+   res.sendFile(path.join(__dirname, "home.html"));
+   visitorCount++;
+ });
+ 
+app.get("/reserve", function(req, res) {
+   res.sendFile(path.join(__dirname, "reserve.html"));
+ });
+ 
+app.get("/tables", function(req, res) {
+   res.sendFile(path.join(__dirname, "tables.html"));
+ });
+
+//API Routing
+
+app.get("/api/table", function (req, res){
+    return res.json(table);
+ });
+ 
+ app.get("/api/waitlist", function (req, res){
+    return res.json(waitlist);
+ });
 
 
 // Starts the server to begin listening

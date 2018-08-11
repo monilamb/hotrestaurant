6cd9c5056e706dd149809309f3cb00ea63aad3de
MoniLamb <monica.lambrano@hotmail.com>
2018-08-11 11:41:54 -0400

    post methods

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Dependencies
 // =============================================================
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 
 // Sets up the Express App
@@ -13,10 +15,15 @@ app.use(bodyParser.json());
 
 //Routing
 var visitorCount = 0;
+var data = {
+    tables: ["", "", "", "", ""],
+    waitlist: []
+};
 
 app.get("/", function(req, res) {
    res.sendFile(path.join(__dirname, "home.html"));
    visitorCount++;
+ 
  });
  
 app.get("/reserve", function(req, res) {
@@ -37,6 +44,39 @@ app.get("/api/table", function (req, res){
     return res.json(waitlist);
  });
 
+ app.post("/api/reservation", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newReservation = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
+    console.log(newReservation);
+
+    for (var i = 0; i < data.tables.length; i++){
+        if (data.tables[i] == ""){
+            data.tables[i] = newReservation
+            console.log("worked");
+            return res.json(data);
+        }
+
+    }
+
+    data.waitlist.push(newReservation);
+    return res.json(data);
+    
+  });
+
+  app.get("/api/clear", function(req, res) {
+    data.tables= ["", "", "", "", ""];
+    data.waitlist.length = 0;
+    res.json(data)
+ })
+ app.get("/api/visitors", function(req, res) {
+    res.json(visitorCount);
+  });
+ 
 
 // Starts the server to begin listening
 // =============================================================
